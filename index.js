63334c259dd887729a6456aa6729e9421efb03c7
edhit <pw6wuukruzdv@mail.ru>
2024-09-19 00:08:41 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is behind 'origin/main' by 4 commits, and can be fast-forwarded.
      (use "git pull" to update your local branch)
    
    Changes to be committed:
    	deleted:    .env.example
    	deleted:    .github/dependabot.yml
    	deleted:    .github/workflows/main.yml
    	modified:   .gitignore
    	deleted:    .husky/pre-commit
    	deleted:    .vscode/extensions.json
    	deleted:    .vscode/settings.json
    	deleted:    README.md
    	deleted:    eslint.config.js
    	new file:   index.js
    	deleted:    locales/en.ftl
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    src/bot/callback-data/change-language.ts
    	deleted:    src/bot/context.ts
    	deleted:    src/bot/features/admin.ts
    	deleted:    src/bot/features/language.ts
    	deleted:    src/bot/features/unhandled.ts
    	deleted:    src/bot/features/welcome.ts
    	deleted:    src/bot/filters/is-admin.ts
    	deleted:    src/bot/handlers/commands/setcommands.ts
    	deleted:    src/bot/handlers/error.ts
    	deleted:    src/bot/helpers/keyboard.ts
    	deleted:    src/bot/helpers/logging.ts
    	deleted:    src/bot/i18n.ts
    	deleted:    src/bot/index.ts
    	deleted:    src/bot/keyboards/change-language.ts
    	deleted:    src/bot/middlewares/session.ts
    	deleted:    src/bot/middlewares/update-logger.ts
    	deleted:    src/config.ts
    	deleted:    src/logger.ts
    	deleted:    src/main.ts
    	deleted:    src/server/environment.ts
    	deleted:    src/server/index.ts
    	deleted:    src/server/middlewares/logger.ts
    	deleted:    src/server/middlewares/request-id.ts
    	deleted:    src/server/middlewares/request-logger.ts
    	deleted:    tsconfig.json

---
+++ index.js
@@ -0,0 +1,87 @@
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+const axios = require("axios");
+
+exports.delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const bot = new Telegraf("7334361333:AAH9f0tf96gkg81M4MjxT48kQmiZDFu1OlM");
+
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.help((ctx) => ctx.reply("Send me a sticker"));
+bot.on(message("sticker"), (ctx) => ctx.reply("👍"));
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+
+bot.on(message("text"), async (ctx) => {
+  const data = ctx.message.text;
+
+  if (data.indexOf(":") === -1) {
+    await ctx.reply("Ошибка! Пиши так: 2:262,263,264");
+    return;
+  }
+
+  const surah = data.split(":")[0];
+  const ayats = data.split(":")[1];
+
+  const ayat = ayats.split(",");
+
+  for (let index = 0; index < ayat.length; index++) {
+    if (Number(ayat[index])) {
+    let bbb
+      let aaa;
+
+      if (surah.length === 1) bbb = `00${surah}`;
+      else if (surah.length === 2) bbb = `0${surah}`;
+      else bbb = `${surah}`;
+
+      if (ayat[index].length === 1) aaa = `00${ayat[index]}`;
+      else if (ayat[index].length === 2) aaa = `0${ayat[index]}`;
+      else aaa = `${ayat[index]}`;
+
+      try {
+        await ctx.replyWithPhoto(
+          `https://cdn.islamic.network/quran/images/high-resolution/${surah}_${ayat[index]}.png`,
+        );
+        await ctx.replyWithAudio(
+            `https://tanzil.net/res/audio/husary/${bbb}${aaa}.mp3`,
+          );
+      } catch (error) {
+        await ctx.reply(`Ошибка в поиске этого аята: ${ayat[index]}. Исправь!!!`);
+        return 
+      }
+    } else{
+        await ctx.reply(`Ошибка в поиске этого аята: ${ayat[index]}. Исправь!!!`);
+        return
+    }
+  }
+
+  //   for (let index = 0; index < ayat.length; index++) {
+  //     // if (Number(ayat[index])) {
+  //     //     let config = {
+  //     //         method: "get",
+  //     //         maxBodyLength: Infinity,
+  //     //         url: `https://api.alquran.cloud/v1/ayah/${surah}:${ayat[index]}/en.asad`,
+  //     //         headers: {
+  //     //           Accept: "application/json",
+  //     //         },
+  //     //       };
+
+  //     //       axios(config)
+  //     //         .then(async (response) => {
+  //     //           const result = response.data;
+  //     //           // console.log(result.data.number);
+
+  //     //           await this.delay(10000)
+  //     //         })
+  //     //         .catch(async (error) => {
+  //     //           await ctx.reply(`Ошибка в поиске этого аята: ${ayat[index]}`);
+  //     //           return;
+  //     //         });
+  //     // }
+  //   }
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
